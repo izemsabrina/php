e42b38397215092dfd98bc3535546bf6c8771280
sabrina <sabrinizem@gmail.com>
2024-01-12 22:18:29 +0100

    les condition

--- index.php
+++ index.php
@@ -23,11 +23,35 @@ print_r($eleve ['notes']);
 
 ?> 
 <?php
-$note = 12;
+$note =(int) readline('entrer la note :');
 if($note >= 10){
   echo 'Bravo vous avez pas la moyenne';
 }else {
     echo 'Dommage vous n\'avez pas la moyenne';
 }
 
+?>
+<?php
+$action = (int) readline('entrer votre action : 1 : attaquer ,  2: defendre, 3: passer mon tour ');
+switch($action){
+    case 1:
+        echo 'jattaque';
+        break;
+    case 2:
+        echo 'je defend';
+        break ;
+    case 3:
+        echo 'passer mon tour ';
+        break ;
+    default:
+        echo 'Commande inconnue';
+// if($action ===1){
+//     echo 'J\attaque';
+// }elseif($action === 2){
+//     echo 'defendre';
+// } else {
+//     echo 'passer mon tour ';
+// }
+
+}
 ?>

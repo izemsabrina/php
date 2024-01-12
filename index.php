4aecfcedfa8045249f2cb9ea9308ba0756ebdadc
sabrina <sabrinizem@gmail.com>
2024-01-12 21:16:21 +0100

    les bases de php

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
-salut tout le monde
 
 <?php
-$nom = 'Sabrina'
+// les chane de carectere
+$note = 15;
+$note2 = 15;
+// echo $note + $note2 /2 ;
+$prenom = "Mechrafi";
+$nom = 'Sabrina';
+
+echo 'bonjour ' .  $nom .' ' .  $prenom . ' vous avez eu  ' .(($note + $note2)/2) .  ' de moyenne'
 ?>
-freferf
+
+<?php
+// les tableaux
+$eleve =
+['nom' => 'sabrina',
+'prenom' => 'mechrafi',
+'notes' =>[12, 13,15]
+];
+
+echo $eleve['nom'];
+print_r($eleve ['notes']);
+
+?> 
+<?php
+$note = 12;
+if($note >= 10){
+  echo 'Bravo vous avez pas la moyenne';
+}else {
+    echo 'Dommage vous n\'avez pas la moyenne';
+}
+
+?>
